9782ff9c9652012665d545ba8fec312247841bfb
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-07 13:18:25 -0500

    Simplify test_api_1

--- Test_API_1.py
+++ Test_API_1.py
@@ -1,18 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/add', methods=['POST'])
-def add_numbers():
-    data = request.get_json()
-    if 'number1' not in data or 'number2' not in data:
-        return jsonify({'error': 'Please provide both number1 and number2'}), 400
-    try:
-        number1 = float(data['number1'])
-        number2 = float(data['number2'])
-    except ValueError:
-        return jsonify({'error': 'Please provide valid numbers'}), 400
-    return jsonify({'result': number1 + number2})
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
