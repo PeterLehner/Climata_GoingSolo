55dcea5b2075bb3da58bd99210bd029080211466
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-08 12:38:38 -0500

    Try making df_working into JSON for #3

--- Test_API_3.py
+++ Test_API_3.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 
 def ShowZipCodes():
     df_working = pd.read_csv('Data/Zips/zip_lat_lon_state_small.csv')
-
     df_working['zip'] = [str(x).zfill(5) for x in df_working['zip']] # Make all zips string and 5 characters
-   
-    return("Test World")
+
+    #df_working into a json
+    df_working = df_working.to_json(orient='records')    
+
+    return(df_working)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
