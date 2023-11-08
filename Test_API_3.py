fdaa1a31bfcfe23887617e6848ade8da38e3300e
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-08 11:41:57 -0500

    Try another path in #3

--- Test_API_3.py
+++ Test_API_3.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 
 def ShowZipCodes():
-    df_working = pd.read_csv('/main/Data/Zips/zip_lat_lon_state_small.csv')
+    df_working = pd.read_csv('Data/Zips/zip_lat_lon_state_small.csv')
 
     df_working['zip'] = [str(x).zfill(5) for x in df_working['zip']] # Make all zips string and 5 characters
    
