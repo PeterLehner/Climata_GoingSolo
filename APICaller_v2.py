411a49fb732740a58c1b52468cea339309e9a557
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-10 12:08:31 -0500

    Change order of return ModelOutput, time_dict

--- APICaller_v2.py
+++ APICaller_v2.py
@@ -16,34 +16,22 @@ def APIRequest():
     electric_bill_query = request.args.get('electric_bill_query')
     sqft_query          = request.args.get('sqft_query')
     heatpump_query      = request.args.get('heatpump_query')
-    t1 = time.time()
 
     electric_bill_query = float(electric_bill_query) if electric_bill_query is not None else None
     sqft_query          = float(sqft_query) if sqft_query is not None else None
-    t2 = time.time()
 
     zip_query = int(zip_query)  # Convert to integer
     dict_working = df_working[df_working['zip'] == zip_query].squeeze().to_dict()  # Read the row with the matching zip_query as a dictionary
-    t3 = time.time()
 
     SavingsModelOutput = SavingsModel(dict_working, zip_query, electric_bill_query, sqft_query, heatpump_query)
+    
     t4 = time.time()
-
-    t1_t0 = t1-t0
-    t2_t1 = t2-t1
-    t3_t2 = t3-t2
-    t4_t3 = t4-t3
     t4_t0 = t4-t0
-
     time_dict = {
-        't1_t0' : t1_t0,
-        't2_t1' : t2_t1,
-        't3_t2' : t3_t2,
-        't4_t3' : t4_t3,
         't4_t0' : t4_t0,
     }
 
-    return time_dict, SavingsModelOutput
+    return SavingsModelOutput, time_dict
 
 if __name__ == '__main__':
     app.run(debug=True)
