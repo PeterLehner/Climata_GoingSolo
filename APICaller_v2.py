47299407090f7f42350b6533283b30bdfaec51e1
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-11 08:21:04 -0500

    Try returning JSON instead of dict

--- APICaller_v2.py
+++ APICaller_v2.py
@@ -12,18 +12,24 @@ app = Flask(__name__)
 def APIRequest():
     t_start = time.time()
     
+    # Get the query parameters from the URL
     zip_query           = request.args.get('zip_query')
     electric_bill_query = request.args.get('electric_bill_query')
     sqft_query          = request.args.get('sqft_query')
     heatpump_query      = request.args.get('heatpump_query')
 
+    # Convert the query parameters to the correct data type
+    zip_query           = int(zip_query) if zip_query is not None else None
     electric_bill_query = float(electric_bill_query) if electric_bill_query is not None else None
     sqft_query          = float(sqft_query) if sqft_query is not None else None
 
-    zip_query = int(zip_query)  # Convert to integer
+    # Filter the dataframe to only the rows with the matching zip_query
     dict_working = df_working[df_working['zip'] == zip_query].squeeze().to_dict()  # Read the row with the matching zip_query as a dictionary
 
     SavingsModelOutput = SavingsModel(dict_working, zip_query, electric_bill_query, sqft_query, heatpump_query)
+
+    #convert to json
+    SavingsModelOutput = SavingsModelOutput.to_json(orient='records')
     
     t_end = time.time()
     run_time = t_start-t_end
