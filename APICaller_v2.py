4996d4107327da57871c76112488d796bce3a229
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-13 15:30:53 -0500

    Turn off debug

--- APICaller_v2.py
+++ APICaller_v2.py
@@ -2,15 +2,10 @@ from flask import Flask, request
 from Main_v101 import SavingsModel
 import pandas as pd
 import time
-from collections import OrderedDict
-
-DEFAULT_LOAN_TERM = 20
-DEFAULT_INTEREST_RATE = 0.06
 
 df_working = pd.read_csv('Data/database_main.csv')
 
 app = Flask(__name__)
-
 @app.route('/')
 
 def APIRequest():
@@ -24,15 +19,15 @@ def APIRequest():
     sqft_query          = request.args.get('sqft_query')
     heatpump_query      = request.args.get('heatpump_query') # "yes" or "no"
 
-    # Convert the query parameters to the correct data type
-    zip_query           = int(zip_query) if zip_query is not None else None
-    electric_bill_query = float(electric_bill_query) if electric_bill_query is not None else None
-    sqft_query          = float(sqft_query) if sqft_query is not None else None
-
     # Error handling
     if zip_query is None:
         return "Error: zip code is None"
-
+    
+    # Correct data types
+    zip_query           = int(zip_query) if zip_query is not None else None
+    electric_bill_query = float(electric_bill_query) if electric_bill_query is not None else None
+    sqft_query          = float(sqft_query) if sqft_query is not None else None
+    
     heatpump_query = heatpump_query if heatpump_query is not None else "no"
 
     dict_working = df_working[df_working['zip'] == zip_query].squeeze().to_dict()  # Read the row with the matching zip_query as a dictionary
@@ -46,4 +41,4 @@ def APIRequest():
     return SavingsModelOutput
 
 if __name__ == '__main__':
-    app.run(debug=True) #TURN OFF DEBUG IN PRODUCTION
+    app.run(debug=False) #TURN OFF DEBUG IN PRODUCTION
