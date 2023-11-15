a3c8dab9ceab89f4463f040c1d3d94299c562511
PeterLehner <84454214+PeterLehner@users.noreply.github.com>
2023-11-14 20:01:09 -0500

    Try redis fix

--- APIengine_04.py
+++ APIengine_04.py
@@ -6,26 +6,33 @@ from CallModel_01 import CallModel
 import os
 import redis
 
-# Connect to your internal Redis instance using the REDIS_URL environment variable
-# The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
+app = Flask(__name__)
 
 REDIS_RENDER_URL = "redis://red-cl9tkmto7jlc73fjaeog:6379"
-#r = redis.from_url(os.environ['REDIS_URL'])
-
 REDIS_LOCAL_URL = "rediss://red-cl9tkmto7jlc73fjaeog:P4Eh45g8zA2NUcBbDvdLV3nXs5bGuyEI@oregon-redis.render.com:6379"
 
-r = redis.from_url(REDIS_RENDER_URL)
+redis_url = REDIS_RENDER_URL
+
+# Connect to your internal Redis instance using the REDIS_URL environment variable
+# The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
+r = redis.from_url(os.environ['REDIS_URL'])
 r.set('key', 'redis-py')
 r.get('key')
 
-app = Flask(__name__)
+# Check Redis connection status
+try:
+    r.ping()
+    print("Connected to Redis successfully!")
+except redis.ConnectionError as e:
+    print(f"Error connecting to Redis: {e}")
 
-# Configure Flask-Limiter with Redis storage URI
 limiter = Limiter(
-    get_remote_address,
-    app=app,
-    default_limits=["1000 per day", "100 per hour"],
-    storage_uri=REDIS_RENDER_URL,
+  get_remote_address,
+  app=app,
+  default_limits=["1000 per day", "1000 per hour"],
+  storage_uri=redis_url,
+  storage_options={"socket_connect_timeout": 30},
+  strategy="fixed-window", # or "moving-window"
 )
 
 @app.route('/trial', methods=['GET'])
@@ -34,7 +41,6 @@ def trial_endpoint():
     api_key = request.headers.get('X-API-Key')
     if not api_key or not validate_trial_key(api_key):
         return "Unauthorized: Invalid API key", 401 
-
     return CallModel()
 
 @app.route('/full', methods=['GET'])
@@ -43,9 +49,8 @@ def full_endpoint():
     api_key = request.headers.get('X-API-Key')
     if not api_key or not validate_full_key(api_key):
         return "Unauthorized: Invalid API key", 401 
-    
     return CallModel()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
